ca8ae16c168ef0e599afd3d6acf85df62e9a606f
TheHott <evgen.kazza@gmail.com>
2023-05-28 12:55:24 +0400

    remove unnecessary @Repository tag

--- src/main/java/com/evgensoft/spring/webshop/clothes/repository/user/UserRepository.java
+++ src/main/java/com/evgensoft/spring/webshop/clothes/repository/user/UserRepository.java
@@ -2,12 +2,9 @@ package com.evgensoft.spring.webshop.clothes.repository.user;
 
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-
 import com.evgensoft.spring.webshop.clothes.model.entity.user.User;
 import com.evgensoft.spring.webshop.clothes.repository.BaseRepository;
 
-@Repository
 public interface UserRepository extends BaseRepository<User, Long> {
     List<User> findAllByUsername(String username);
 }
